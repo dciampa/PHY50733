130f3ea594879b777e79b6a6e21c19a31ae045c1
dciampa <drewciampa@yahoo.com>
2017-02-07 14:33:39 -0600

    Update Ciampa_hw2_prob1.py

--- Ciampa_hw2_prob1.py
+++ Ciampa_hw2_prob1.py
@@ -10,10 +10,11 @@ c=3e10
 
 #function that will calculate the time it takes for the ship to reach the planet observed from Earth.
 def timeEarth(dist_inp,beta):
-	dist=dist_inp*9.5e18  #puts light-years into cm
+	dist=dist_inp*9.461e17  #puts light-years into cm
 	vel=beta*c            #puts velocity into cm/s
 	
 	t_earth = dist/vel
+	t_earth=t_earth/31536000.
 	
 	print("At",dist_inp,"Light-years...")
 	print("The time it takes to travel there is %.2E sec for an observer on Earth." % Decimal(t_earth))
@@ -21,12 +22,13 @@ def timeEarth(dist_inp,beta):
 
 #function that will calculate the time it takes for the ship to reach the planet observed by a passenger
 def timeShip(dist_inp,beta):
-	dist=dist_inp*9.5e18  #puts light-years into cm
+	dist=dist_inp*9.461e17  #puts light-years into cm
 	vel=beta*c            #puts velocity into cm/s
 	gamma=1.0/(np.sqrt(1.0-(beta**2)))   #gamma factor accounts for the length contraction due to relativistic speeds in frame
 
 	distance=dist/gamma
 	t_ship=distance/vel
+	t_ship=t_ship/31536000.
 	
 	print("At",dist_inp,"Light-years...")
 	print("The time it takes to travel there is %.2E sec for an observer on the spaceship." % Decimal(t_ship))
