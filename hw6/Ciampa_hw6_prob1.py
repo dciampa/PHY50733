b882745958499104be7f41fa278806433d00598c
Drew Ciampa <Drewciampa@yahoo.com>
2017-04-16 15:13:43 -0500

    modify hw6

--- hw6/Ciampa_hw6_prob1.py
+++ hw6/Ciampa_hw6_prob1.py
@@ -19,26 +19,28 @@ h = (b-a)/N
 theta=-176
 
 tpoints = np.arange(a,b,h)
-theta = []
+thetapoints = []
 
 def f(r,t):
     theta = r[0]
     omega = r[1]
-    ftheta = omega
-    fomega = -g/l*np.sin(theta)
-    return np.array([ftheta,fomega],float)
+    dtheta = omega
+    domega = -g/l*np.sin(theta)
+    return np.array([dtheta,domega],float)
 
 #r = np.array([-176/180*pi,0],float)
 r = np.array([theta*np.pi/180,0],float)
 for t in tpoints:
-    theta.append(r[0])
+    thetapoints.append(r[0])
     k1 = h*f(r,t)
     k2 = h*f(r+0.5*k1,t+0.5*h)
     k3 = h*f(r+0.5*k2,t+0.5*h)
     k4 = h*f(r+k3,t+h)
     r += (k1+2*k2+2*k3+k4)/6
 
-plt.plot(tpoints,theta)
+
+thetapoints = 180/np.pi * np.array(thetapoints)
+plt.plot(tpoints,thetapoints)
 #plt.plot(tpoints,ypoints)
 plt.xlabel("t")
 plt.ylabel("theta")
