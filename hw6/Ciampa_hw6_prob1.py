06740444c1edfdf1e85c6c51eb6a6f95e333d815
Drew Ciampa <Drewciampa@yahoo.com>
2017-04-16 14:25:55 -0500

    modify hw6

--- hw6/Ciampa_hw6_prob1.py
+++ hw6/Ciampa_hw6_prob1.py
@@ -5,8 +5,7 @@ Created on Fri Apr  7 19:31:43 2017
 @author: Drew
 """
 
-from __future__ import division, print_function
-from pylab import *
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -39,21 +38,9 @@ for t in tpoints:
     k4 = h*f(r+k3,t+h)
     r += (k1+2*k2+2*k3+k4)/6
 
-plot(tpoints,theta)
-#plot(tpoints,ypoints)
-xlabel("t")
-ylabel("theta")
-show()
-
-    for t in tpoints:
-    
-        xpoints.append(r[0])
-        ypoints.append(r[1])
-        
-        k1 = h*f(r,t)
-        k2 = h*f(r+0.5*k1,t+0.5*h)
-        k3 = h*f(r+0.5*k2,t+0.5*h)
-        k4 = h*f(r+k3,t+h)
-        
-        r += (k1+2*k2+2*k3+k4)/6
+plt.plot(tpoints,theta)
+#plt.plot(tpoints,ypoints)
+plt.xlabel("t")
+plt.ylabel("theta")
+plt.show()
 
